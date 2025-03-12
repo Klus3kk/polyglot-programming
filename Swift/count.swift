57559009c7eddecdabbfe56a82b98730887f8032
Klus3kk <lukasz.bielaszewski@onet.pl>
2025-03-12 21:10:34 +0100

    Swift.

---
+++ Swift/count.swift
@@ -0,0 +1,5 @@
+print("Counting from 1 to 10:")
+
+for i in 1...10 {
+    print(i)
+}
---
+++ Swift/inout.swift
@@ -0,0 +1,5 @@
+print("Please enter your name: ", terminator: "")
+
+if let name = readLine() {
+    print("Hello, \(name)")
+}
