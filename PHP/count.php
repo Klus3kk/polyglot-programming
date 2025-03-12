cb7a3f60fe00662c5310552adabffb8ef19c5433
Klus3kk <lukasz.bielaszewski@onet.pl>
2025-03-12 21:07:38 +0100

    PHP

---
+++ PHP/count.php
@@ -0,0 +1,7 @@
+<?php 
+echo "Counting from 1 to 10:\n"
+
+for($i = 1; $i <= 10; $i++) {
+    echo $i . "\n";
+}
+?>
---
+++ PHP/inout.php
@@ -0,0 +1,8 @@
+<?php
+echo "Please enter your name: ";
+
+// For command-line PHP
+$name = trim(fgets(STDIN));
+
+echo "Hello, $name\n";
+?>
